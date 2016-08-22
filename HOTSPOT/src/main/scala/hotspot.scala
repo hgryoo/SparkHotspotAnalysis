e040fae7fc9cc0e9d8ace89452c235cd8475c920
cocoslime <thmlion24@gmail.com>
2016-08-22 14:07:30 +0900

    hotspot last version

--- HOTSPOT/src/main/scala/hotspot.scala
+++ HOTSPOT/src/main/scala/hotspot.scala
@@ -4,11 +4,12 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import scala.math._
 import org.apache.spark.storage.StorageLevel
+import org.apache.spark.HashPartitioner
 import scala.collection.mutable._
 import java.io._
 import java.lang.System._
 
-object neighbor{
+object hotspot{
   def find_days(para : Int) : Int = {
 	var x = 0
 	var total_days = 0
@@ -33,23 +34,19 @@ object neighbor{
   def main(args: Array[String]): Unit = {
 
 
-	val time_start = java.lang.System.currentTimeMillis();
 	val degree= args(0).toDouble
 	val time_step = args(1).toInt
 	val input_path = args(2)
 	val result_path = args(3)
-	val top_percent = args(4).toDouble
+	val top_percent = 0.10
 
-	val PART = 100
-   // System.setProperty("hadoop.home.dir", "HADOOP_HOME");
-    
 	val conf = new SparkConf().
-	setAppName("neighbor")
+	setAppName("hotspot")
 	.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
 	.set("spark.kryo.registrationRequired","true")
 	.registerKryoClasses(Array(
 	classOf[Array[String]],
-	neighbor.getClass,
+	hotspot.getClass,
 	classOf[java.lang.Class[_]],
 	classOf[Array[Int]],
 	classOf[scala.reflect.ClassTag$$anon$1],
@@ -64,19 +61,11 @@ object neighbor{
 
 	val result_filter = data.filter(line => line(0) != "VendorID").filter(line => line(5) != "0")
 		.filter(line => line(6) != "0").filter(line => line(9) != "0").filter(line => line(10) != "0")
-		//.persist(StorageLevel.MEMORY_ONLY)
-
-	
-	
 	val result3 = result_filter.flatMap( line => {
 			Array( (line(6),line(5),line(1) ), (line(10),line(9),line(2) ) )
 			
 			}
 )
-	//val seq_rdd = Seq(result_pick, result_drop)
-	//val result3 = sc.union(seq_rdd)
-
-	
 	val result4 = result3.map( line => {
 		(
 			( 
@@ -121,27 +110,24 @@ object neighbor{
 	val broad_map = sc.broadcast(result5_map);
     
 	val sort_result5= result5_map.toSeq.sortWith(_._2 > _._2)
-	//val sort_result5 = result5.sortBy(_._2, false)
+
 	val neighbor_value = 1
 
-	val g_rdd = sc.parallelize(sort_result5.take(top_amount) , top_amount * 0.002).map{ x =>
-	//val g_rdd = sort_result5.take(top_amount).map { x =>
+	val g_rdd = sc.parallelize(sort_result5.take(top_amount) , (top_amount * 0.002).toInt).map{ x =>
 		{
 			
 			var sum_value = 0.0;
-			var sum_neighbor = 0.0;
+			val sum_neighbor = 27;
 			var i = 0
 			var j = 0
 			var k = 0
 			for ( i <- -neighbor_value to neighbor_value){
 				for ( j <- -neighbor_value to neighbor_value ) {
 					for ( k <- -neighbor_value to neighbor_value ) {
-						var max = abs(i)
-						if ( max < abs(j) ) { max = abs(j) }
-						if ( max < abs(k) ) { max = abs(k) }
+						
 						if (broad_map.value.get(x._1._1 + i, x._1._2 + j, x._1._3 + k) != None){
 							sum_value += broad_map.value(x._1._1 + i, x._1._2 + j, x._1._3 + k);
-							sum_neighbor += 1;
+				
 						}
 					}
 				}
@@ -159,40 +145,25 @@ object neighbor{
 	}
 
 	
-		
-     // val res = (0.1,0.1,0.1)
-    
-		 // weight_map += ((line._1._1,line._1._2,line._1._3) -> (res._1,res._2,res._3))
-		
-    
-	println("S : " + S);
-	println("mean : " + mean);
-	println("rdd_size : " + cube_size);
-	println("real num :" + data_num.value );
-	println("g_rdd")
 	val sort_g = g_rdd.collect.toSeq.sortWith(_._2 > _._2)
 	
-	
-	sort_g.take(50).foreach(println(_))
 
-	val time_finish = java.lang.System.currentTimeMillis();
-	val time = time_finish - time_start;
-
-	val writer = new PrintWriter(new File(result_path + "/result_"+args(0)+
-				"_"+args(1)+"_"+ args(4)+"_neighbor_one_v0.0.1.txt"))
-
+	val writer = new PrintWriter(new File(result_path + "/result_hotspot.csv"))
+	writer.write("cell_x, cell_y, time_step, zscore, pvalue \n");
+	var p_value = 1.0;
 
 	sort_g.take(50).foreach{
-			line => writer.write((line._1._1) + ", " + (line._1._2) + ", " +
-			(line._1._3) + " : " + (line._2))
+			line => { 
+			writer.write((line._1._1) + ", " + (line._1._2) + ", " +(line._1._3) + ", " + (line._2) + ", ")
+			writer.write((p_value / cube_size.toDouble).toString)
+			p_value += 1;
 			writer.write("\n")
+		}
 	}
-	writer.write(time.toString);
-	writer.write("\n data num : "+data_num.value);
-	println(time);
+
 	writer.close();
 	
-	readChar
+
   }
 }
 
